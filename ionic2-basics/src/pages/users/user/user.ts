5e173c2a9ae9c2b6677af0c08241d4fba78dcecc
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-12 13:02:22 -0300

    section 2 lecture 27

--- ionic2-basics/src/pages/users/user/user.ts
+++ ionic2-basics/src/pages/users/user/user.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {NavParams,NavController} from 'ionic-angular';
 @Component({
     selector: 'page-user',
     templateUrl: 'user.html'
 })
 export class UserPage {
     name: string;
+    constructor(private navParams:NavParams, private navCtrl:NavController){}
+    ngOnInit(){
+        this.name = this.navParams.get('userName');
+    }
+    onGoBack(){
+        //this.navCtrl.pop();
+        this.navCtrl.popToRoot();
+    }
 }
